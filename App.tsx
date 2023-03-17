f797da5daa27c8b684b90be273376013fe7580c7
Rafael Kauch <kauchrafael0001@gmail.com>
2023-03-17 11:18:50 -0300

    integrando V-Stack, e adicionando Loading ao carregar fonts.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { THEME } from './src/theme';
+import { Loading } from '@components/Loading';
+import { SignIn } from '@screens/Signin';
 
 export default function App() {
+  const [fontsloaded] = useFonts({ Roboto_400Regular, Roboto_700Bold}) 
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {fontsloaded ? <SignIn/> : <Loading/>}
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
