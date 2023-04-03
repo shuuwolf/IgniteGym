0052f51deeaca64beb6587e069fbf4ec9c5ee6e1
Rafael Kauch <shuuwolf.home@gmail.com>
2023-04-03 18:34:42 -0300

    adding emptyList Message

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,14 +1,49 @@
-import { VStack } from "native-base";
+import { useState } from 'react';
+import { VStack, Heading, SectionList, Text } from "native-base";
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { HistoryCard } from "@components/HistoryCard";
 
+
 export function History(){
+    const [exercises, setExercises] = useState([
+        {
+            title: "26.08.22",
+            data: ["Puxada frontal"]
+        },
+
+        {
+            title: "26.08.23",
+            data: ["Arremesso de Machado"]
+        }
+    ]);
+
     return (
         <VStack flex={1}>
             <ScreenHeader title="Historico de Exercicios"/>
 
-            <HistoryCard />
+            <SectionList
+                sections={exercises}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <HistoryCard />
+                )}   
+                renderSectionHeader={({ section }) => (
+                    <Heading color="gray.200" fontSize="md" mt={10} mb={3}> 
+                        {section.title}
+                    </Heading>
+                )}    
+                px={8}  
+                contentContainerStyle={exercises.length === 0 && {flex: 1, justifyContent: 'center'}}
+                ListEmptyComponent={() => (
+                    <Text color="gray.100" textAlign="center">
+                        Nao ha exercicios registrados ainda.{'\n'}
+                        Vamos fazer exercicios hoje?
+                    </Text>
+                )}   
+            />
+
+            
         </VStack>
     )
 }
